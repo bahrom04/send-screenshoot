3c06399aa735f40deb97272ca3388f876639ff8c
bahrom04 <magdiyevbahrom@gmail.com>
2024-07-22 18:36:24 +0500

    update staticfiles

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost"
+]
